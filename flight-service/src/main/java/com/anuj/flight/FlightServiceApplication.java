1c87a907190d1dec8d7b603ed4e4b7316b04841a
Ubuntu <ubuntu@ip-172-31-15-61.eu-north-1.compute.internal>
2026-04-13 15:02:51 +0000

    Hard disable Eureka auto-configuration for standalone deployment

--- flight-service/src/main/java/com/anuj/flight/FlightServiceApplication.java
+++ flight-service/src/main/java/com/anuj/flight/FlightServiceApplication.java
@@ -2,11 +2,19 @@ package com.anuj.flight;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EurekaClientAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.EurekaDiscoveryClientConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(
+    exclude = {
+        EurekaClientAutoConfiguration.class,
+        EurekaDiscoveryClientConfiguration.class
+    }
+)
 public class FlightServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(FlightServiceApplication.class, args);
     }
 }
+
